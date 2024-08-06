1deaeb0802f5ea6efedbe697d79a761760e3014c
zauser519 <56807402+zauser519@users.noreply.github.com>
2024-08-07 08:33:21 +0900

    Update DLL.cpp

--- Task1-3/DLLTest2/DLL.cpp
+++ Task1-3/DLLTest2/DLL.cpp
@@ -1,50 +1,40 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
-#include "DLL.h"
 #include <gtest/gtest.h>
+#include "DLL.h"
 
-// PerformanceData 構造体の定義
-struct PerformanceData {
-    int score = 0; // スコアの初期化
-    std::string username;
+using PerformanceData = std::pair<int, std::string>;
 
-    friend std::ostream& operator<<(std::ostream& os, const PerformanceData& data) {
-        os << "Score: " << data.score << ", Username: " << data.username;
-        return os;
+void displayList(const DoublyLinkedList<PerformanceData>& list) {
+    for (auto it = list.begin(); it != list.end(); ++it) {
+        std::cout << "Score: " << it->first << ", User Name: " << it->second << std::endl;
     }
-
-    bool operator==(const PerformanceData& other) const {
-        return score == other.score && username == other.username;
-    }
-
-    bool operator!=(const PerformanceData& other) const {
-        return !(*this == other);
-    }
-};
+}
 
 int main(int argc, char** argv) {
-    DoublyLinkedList<PerformanceData> list; // DoublyLinkedList はテンプレートです。
+    DoublyLinkedList<PerformanceData> list;
     std::ifstream infile("Scores.txt");
 
     if (infile.is_open()) {
         std::string line;
         while (std::getline(infile, line)) {
             std::istringstream iss(line);
-            PerformanceData data;
-            if (!(iss >> data.score >> data.username)) {
+            int score;
+            std::string username;
+            if (!(iss >> score >> username)) {
                 break;
             }
-            list.addNode(data); // データを追加
+            list.addNode({ score, username });
         }
         infile.close();
     }
     else {
-        std::cerr << "Cannot open file" << std::endl; // ファイルが開けない場合
+        std::cerr << "Cannot open file" << std::endl;
     }
 
-    list.displayList(); // リストを表示
+    displayList(list);
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
