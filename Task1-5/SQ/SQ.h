3af82da2f181385a995525e1488308ed8a6f25d2
zauser519 <56807402+zauser519@users.noreply.github.com>
2024-08-09 17:40:32 +0900

    Update SQ.h

--- Task1-5/SQ/SQ.h
+++ Task1-5/SQ/SQ.h
@@ -1,8 +1,71 @@
-﻿#ifndef SQ_H
+#ifndef SQ_H
 #define SQ_H
 
 #include <stdexcept>
-#include "DLL.h"
+
+/**
+ * @brief テンプレートクラス DoublyLinkedList
+ * ダブルリンクリストの実装
+ */
+template<typename T>
+class DoublyLinkedList {
+private:
+    struct Node {
+        T data;
+        Node* prev;
+        Node* next;
+        Node(const T& rd);
+    };
+
+    Node* head;
+    Node* tail;
+    int size;
+
+    void quickSort(Node* left, Node* right, const std::function<bool(const T&, const T&)>& comp);
+    Node* partition(Node* left, Node* right, const std::function<bool(const T&, const T&)>& comp);
+    void swap(T& data1, T& data2);
+
+public:
+    class ConstIterator {
+    private:
+        Node* current;
+        const DoublyLinkedList* list;
+        friend class DoublyLinkedList;
+
+    public:
+        ConstIterator(Node* node, const DoublyLinkedList* list);
+        ConstIterator(const ConstIterator& other);
+        ConstIterator& operator--();
+        ConstIterator operator--(int);
+        ConstIterator& operator++();
+        ConstIterator operator++(int);
+        const T& operator*() const;
+        void operator=(const ConstIterator& other);
+        bool operator==(const ConstIterator& other) const;
+        bool operator!=(const ConstIterator& other) const;
+    };
+
+    class Iterator : public ConstIterator {
+    public:
+        Iterator(Node* node, const DoublyLinkedList* list);
+        Iterator& operator--();
+        Iterator operator--(int);
+        Iterator& operator++();
+        Iterator operator++(int);
+        T& operator*();
+    };
+
+    DoublyLinkedList();
+    int Getsize() const;
+    bool Insert(const Iterator& iter, const T& data);
+    bool Delete(const Iterator& iter);
+    Iterator begin();
+    ConstIterator beginConst() const;
+    Iterator end();
+    ConstIterator endConst() const;
+    void Sort(const std::function<bool(const T&, const T&)>& comp);
+    ~DoublyLinkedList();
+};
 
 /**
  * @brief スタッククラス
@@ -10,7 +73,7 @@
 template<typename T>
 class Stack {
 private:
-    DoublyLinkedList<T> list; // 内部リストとして二重連結リストを使用
+    DoublyLinkedList<T> list; // 内部リストとして新しい二重連結リストを使用
 
 public:
     Stack(); // コンストラクタ
@@ -27,7 +90,7 @@ public:
 template<typename T>
 class Queue {
 private:
-    DoublyLinkedList<T> list; // 内部リストとして二重連結リストを使用
+    DoublyLinkedList<T> list; // 内部リストとして新しい二重連結リストを使用
 
 public:
     Queue(); // コンストラクタ
